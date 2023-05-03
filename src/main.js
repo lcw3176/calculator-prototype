b7a4099303e06a6d7a7fc796ac65449d16bdf772
lcw3176 <lcw3176@naver.com>
2023-05-03 17:54:17 +0900

    feat: vuetify 적용

--- src/main.js
+++ src/main.js
@@ -2,6 +2,18 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import './registerServiceWorker'
-import { createPinia } from "pinia";
+import { createPinia } from "pinia"
 
-createApp(App).use(createPinia()).use(router).mount('#app')
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+const vuetify = createVuetify({
+  components,
+  directives,
+})
+
+
+createApp(App).use(createPinia()).use(router).use(vuetify).mount('#app')
