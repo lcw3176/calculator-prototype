4fd4642408ff031c43dddf3bd58eb666a89d2aa3
lcw3176 <lcw3176@naver.com>
2023-05-22 14:58:47 +0900

    refactor: 기존 서비스 워커 사용

--- public/worker.js
+++ public/worker.js
@@ -1,47 +1,47 @@
-var cacheName = 'meal-deal-v1';
-var appShellFiles = [
-  'food.json',
-  '/icons/icon-192x192.png',
-  '/icons/icon-256x256.png',
-  '/icons/icon-384x384.png',
-  '/icons/icon-512x512.png',
-];
+// var cacheName = 'meal-deal-v1';
+// var appShellFiles = [
+//   'food.json',
+//   '/icons/icon-192x192.png',
+//   '/icons/icon-256x256.png',
+//   '/icons/icon-384x384.png',
+//   '/icons/icon-512x512.png',
+// ];
 
-var contentToCache = appShellFiles;
+// var contentToCache = appShellFiles;
 
-self.addEventListener('install', (e) =>{
+// self.addEventListener('install', (e) =>{
 
-    console.log("[Service Worker] Install");
-    e.waitUntil(
-      (async () => {
-        const cache = await caches.open(cacheName);
-        console.log("[Service Worker] Caching all: app shell and content");
-        await cache.addAll(contentToCache);
-      })()
-    );
-})
+//     console.log("[Service Worker] Install");
+//     e.waitUntil(
+//       (async () => {
+//         const cache = await caches.open(cacheName);
+//         console.log("[Service Worker] Caching all: app shell and content");
+//         await cache.addAll(contentToCache);
+//       })()
+//     );
+// })
 
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then(function(r) {
-          console.log('[Service Worker] Fetching resource: '+e.request.url);
-      return r || fetch(e.request).then(function(response) {
-                return caches.open(cacheName).then(function(cache) {
-          console.log('[Service Worker] Caching new resource: '+e.request.url);
-          cache.put(e.request, response.clone());
-          return response;
-        });
-      });
-    })
-  );
-})
+// self.addEventListener('fetch', (e) => {
+//   e.respondWith(
+//     caches.match(e.request).then(function(r) {
+//           console.log('[Service Worker] Fetching resource: '+e.request.url);
+//       return r || fetch(e.request).then(function(response) {
+//                 return caches.open(cacheName).then(function(cache) {
+//           console.log('[Service Worker] Caching new resource: '+e.request.url);
+//           cache.put(e.request, response.clone());
+//           return response;
+//         });
+//       });
+//     })
+//   );
+// })
 
-self.addEventListener('activate', () => {
-  self.clients.matchAll({ type: 'window' }).then(windowClients => {
-    for (const windowClient of windowClients) {
-      // Force open pages to refresh, so that they have a chance to load the
-      // fresh navigation response from the local dev server.
-      windowClient.navigate(windowClient.url)
-    }
-  })
-})
+// self.addEventListener('activate', () => {
+//   self.clients.matchAll({ type: 'window' }).then(windowClients => {
+//     for (const windowClient of windowClients) {
+//       // Force open pages to refresh, so that they have a chance to load the
+//       // fresh navigation response from the local dev server.
+//       windowClient.navigate(windowClient.url)
+//     }
+//   })
+// })
